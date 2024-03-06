8e736ae7fb5ca44f5e3896b5347e3495f821705c
Čapka Michal <michal.capka@i.cz>
2024-03-06 12:56:05 +0100

    #user-restController created UserController added /{id} endpoint

---
+++ src/main/java/org/example/task/controller/UserController.java
@@ -0,0 +1,26 @@
+package org.example.task.controller;
+
+import org.example.task.dto.UserDto;
+import org.example.task.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        var userDto = userService.getUserById(id);
+        return ResponseEntity.ok(userDto);
+    }
+}
