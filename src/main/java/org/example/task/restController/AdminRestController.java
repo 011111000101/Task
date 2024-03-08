024ae94124648bc2343312fd06ed9782b2f5d44d
Čapka Michal <michal.capka@i.cz>
2024-03-08 16:34:45 +0100

    #security-settings added basic security settings for endpoints added admin credentials to properties

--- src/main/java/org/example/task/restController/AdminRestController.java
+++ src/main/java/org/example/task/restController/AdminRestController.java
@@ -3,23 +3,25 @@ package org.example.task.restController;
 import org.example.task.dto.BasicResponseDto;
 import org.example.task.dto.ResponseDto;
 import org.example.task.service.UserService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminRestController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
-    public AdminRestController(UserService userService) {
+    public AdminRestController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
     }
     @DeleteMapping
-    public ResponseEntity<? extends ResponseDto> deleteUser(@RequestParam Long id) {
+    public ResponseEntity<? extends ResponseDto> deleteUser(@RequestParam Long id, @RequestHeader HttpHeaders headers) {
         userService.delete(id);
         return ResponseEntity.ok(new BasicResponseDto("User deleted with ID: " + id));
     }
